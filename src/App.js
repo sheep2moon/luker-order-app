bf14d736d93aa391f6bbac7cb8fcbcdfbd08c9ca
sheep2moon <kon.iwan17@gmail.com>
2021-05-19 23:04:49 +0200

    creating cart context

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import Home from './pages/Home';
 import Shop from './pages/Shop';
 import AuthProvider from './contexts/AuthContext';
 import AddProduct from './components/AddProduct';
+import CartProvider from './contexts/CartContext';
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <Layout>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/shop' component={Shop} />
-            <Route exact path='/add-product' component={AddProduct} />
-          </Switch>
-        </Layout>
+        <CartProvider>
+          <Layout>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/shop' component={Shop} />
+              <Route exact path='/add-product' component={AddProduct} />
+            </Switch>
+          </Layout>
+        </CartProvider>
       </AuthProvider>
     </Router>
   );
---
+++ src/contexts/CartContext.js
@@ -0,0 +1,19 @@
+import React, { useContext, useState } from 'react';
+
+const CartContext = React.createContext();
+
+export function useCart() {
+  return useContext(CartContext);
+}
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState({});
+
+  const value = {
+    cart,
+    setCart,
+  };
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+
+export default CartProvider;
