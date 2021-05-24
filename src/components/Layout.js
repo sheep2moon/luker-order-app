9c899fd11ccea36ac7460e07565a52f4f8cb933f
sheep2moon <kon.iwan17@gmail.com>
2021-05-24 15:53:59 +0200

    creating mobile menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import cartIcon from '../assets/cart.svg';
 import '../styles/layout.scss';
 import logoIcon from '../assets/logo.svg';
+import menuIcon from '../assets/menu.svg';
 import { useAuth } from '../contexts/AuthContext';
 import { useCart } from '../contexts/CartContext';
 
 const Layout = ({ children }) => {
   const { cart } = useCart();
   const { login, user } = useAuth();
+  const [mobileMenuActive, setMobileMenuActive] = useState(false);
   if (!user) {
     login();
   }
@@ -19,6 +21,41 @@ const Layout = ({ children }) => {
           <img src={logoIcon} alt='' width='50' />
           <p>Luker</p>
         </div>
+        <div className='desktop-menu'>
+          <ul>
+            <li>
+              <Link to='/'>Strona Główna</Link>
+            </li>
+            <li>
+              <Link to='/shop'>Oferta</Link>
+            </li>
+            <li>
+              <Link to='/'>Kontakt</Link>
+            </li>
+            <li>
+              <Link to='/add-product'>Dodaj produkt</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='mobile-menu-button'>
+          <img
+            src={menuIcon}
+            alt=''
+            onClick={() => setMobileMenuActive(!mobileMenuActive)}
+          />
+        </div>
+
+        <div className='cart-icon'>
+          <p>{cart.length}</p>
+          <Link to='/cart'>
+            <img src={cartIcon} alt='' width='50' />
+          </Link>
+        </div>
+      </nav>
+      <div
+        style={mobileMenuActive ? { display: 'block' } : { display: 'none' }}
+        className='mobile-menu-links'
+      >
         <ul>
           <li>
             <Link to='/'>Strona Główna</Link>
@@ -33,13 +70,7 @@ const Layout = ({ children }) => {
             <Link to='/add-product'>Dodaj produkt</Link>
           </li>
         </ul>
-        <Link to='/cart'>
-          <div className='cart-icon'>
-            <span>{cart.length}</span>
-            <img src={cartIcon} alt='' width='50' />
-          </div>
-        </Link>
-      </nav>
+      </div>
       {children}
     </>
   );
