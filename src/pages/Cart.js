d593bda6ae8409fd4a9dacec4f2829b0fea583fe
sheep2moon <kon.iwan17@gmail.com>
2021-06-01 13:23:36 +0200

    cart styling

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useCart } from '../contexts/CartContext';
+import '../styles/cartPage.scss';
 
 const Cart = () => {
   const { cart, setCart } = useCart();
   return (
-    <div>
+    <div className='cart-container'>
       {cart.map((product) => (
         <div className='cart-item'>
           <img src={product.image} alt='' width='50' />
